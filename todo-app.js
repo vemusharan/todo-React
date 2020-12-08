ea3f916059fca9b37f94696dbddad007cdd6e8e1
sharan <sai.vemu@st.ovgu.de>
2020-12-08 23:31:53 +0100

    Dynamically updating time

--- todo-app.js
+++ todo-app.js
@@ -1,4 +1,19 @@
-ReactDOM.render(
-	<h1> Hello,Sharan.Vemu </h1>,
-	document.getElementById('root')
-	);
+function tick() {
+	console.log(name)
+	const element = (
+		<div>
+			
+			<h1> Hello {name} </h1>
+			<h2> Today is {new Date().toLocaleTimeString()} </h2>
+
+
+		</div>
+		);
+				ReactDOM.render(
+		element,document.getElementById('root'));
+
+}
+setInterval(tick,1000);
+
+
+
