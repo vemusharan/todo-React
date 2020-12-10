9f63eaf5eb49add9d95ff581e58f3457ae17d074
sharan <sai.vemu@st.ovgu.de>
2020-12-10 01:43:45 +0100

    added components

--- todo-app.js
+++ todo-app.js
@@ -1,19 +1,63 @@
-function tick() {
-	console.log(name)
-	const element = (
-		<div>
+// function tick() {
+// 	console.log(name)
+// 	const element = (
+// 		<div>
 			
-			<h1> Hello {name} </h1>
-			<h2> Today is {new Date().toLocaleTimeString()} </h2>
+// 			<h1> Hello {name} </h1>
+// 			<h2> Today is {new Date().toLocaleTimeString()} </h2>
 
 
-		</div>
-		);
-				ReactDOM.render(
-		element,document.getElementById('root'));
+// 		</div>
+// 		);
+// 				ReactDOM.render(
+// 		element,document.getElementById('root'));
+
+// }
+// setInterval(tick,1000);
+
+
+// function Welcome(props) {
+// 	return <h1> Hello , {props.name} </h1>;
+// }
+
+// function App() {
+// 	return (
+// 	<div>
+// 	<Welcome name= 'Sharan' />
+// 	<Welcome name= 'Vamshi' />
+// 	</div>
+// 	);
+// }
+
+// ReactDOM.render(
+// <App/>,
+// document.getElementById('root')
+// );
+
+class Todolist extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1> TODO list </h1>
+				<label for="startdate">Enter Task:</label>
+				<input type="text" name="taskname" id="taskid" />
+					<br/>
+				<label for="startdate">Start Date:</label>
+					<input type="date" />
+					<br/>
+				<label for="enddate">End Date:</label>
+				<input type="date" />
+				<br/>
+				<button onclick="printText()"> Add Task </button>	
+				{new Date().toLocaleTimeString()}
+
+			
+			</div>
+			);
+	}
 
 }
-setInterval(tick,1000);
-
-
-
+ReactDOM.render(
+<Todolist />,
+document.getElementById('root')
+);
